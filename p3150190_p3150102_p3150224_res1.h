52ed9a86064ba6212bc5c825da1a1c0f0f0783bb
giannisfourfouris <giannisfourfouris@gmail.com>
2019-04-15 18:47:10 +0300

    test-resh1

--- p3150190_p3150102_p3150224_res1.h
+++ p3150190_p3150102_p3150224_res1.h
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
-int Nseat=250; 
-int Ntel=8; 
+int Ntel=8;
+int Ncash=4;
+int Nseat=10;
+int NzoneA=5;
+int NzoneB=10;
+int NzoneC=10;
+int PzoneA=2;
+int PzoneB=6;
+int PzoneC=10;
+int CzoneA=30;
+int CzoneB=25;
+int CzoneC=20;
 int Nseatlow=1; 
 int Nseathigh=5; 
 int tseatlow=5;
 int tseathigh=10; 
+int tcashlow=2;
+int tcashhigh=4;
 int Pcardsuccess=9;
+
+//afto tha vgei!!
 int Cseat=20;
+
 
---
+++ test-res1.sh
@@ -0,0 +1,2 @@
+gcc -Wall -pthread p3150190_p3150102_p3150224_res1.c
+./a.out 100 1000
